2c7c62c2d924ea3f792ec4f1767894deb7d0109e
Djonahuti <86109337+Djonahuti@users.noreply.github.com>
2024-09-16 20:12:59 +0100

    commit

---
+++ user/use/nav.php
@@ -0,0 +1,43 @@
+
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <i class="bi bi-fingerprint"></i>Jonah's GAUS</a>
+        <div class="btn-group">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
+        </button></div>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav text-uppercase ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#" title="Shop With Us" style="font-family: 'Lobster Two', cursive">Store
+                </a>
+              </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" title="Learn programming with our Tutorial Videos Guides" style="font-family: 'Lobster Two', cursive">
+                Become a Programmer
+                </a>
+              </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#" title="Meet Our Team" style="font-family: 'Lobster Two', cursive">Team</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#" title="Contact Us" style="font-family: 'Lobster Two', cursive">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#" title="About Us" style="font-family: 'Lobster Two', cursive">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#" title="The Service We Provide" style="font-family: 'Lobster Two', cursive">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="user.php" title="Manage your Profile" style="font-family: 'Lobster Two', cursive">
+                <i class="fas fa-user-alt" style="font-size: 28px;"></i><?php echo $rname; ?></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
